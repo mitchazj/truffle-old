eeb7aee4022ade733e2000e23751acdcdd3b10d8
Mitchell Johnson <orangutandevelopment@gmail.com>
2016-07-08 06:45:24 +1000

    Bug Fixes (#2, #3, #7)
    Fairly hackish fixes. I need to do some refactoring before the next
    release.

--- Blacksink/Blacksink/Program.cs
+++ Blacksink/Blacksink/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,15 +10,27 @@ namespace Blacksink
     static class Program
     {
         /// <summary>
+        /// Helps us to ensure only one instance runs at a time.
+        /// </summary>
+        static Mutex mutex = new Mutex(true, "{0fbc294c-f089-4009-9b1a-ab757739483f}");
+
+        /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main() {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            CefSharp.Cef.EnableHighDPISupport();
-            Application.Run(new MainContext());
-            CefSharp.Cef.Shutdown();
+            if (mutex.WaitOne(TimeSpan.Zero, true)) {
+                try {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    CefSharp.Cef.EnableHighDPISupport();
+                    Application.Run(new MainContext());
+                    CefSharp.Cef.Shutdown();
+                }
+                finally {
+                    mutex.ReleaseMutex();
+                }
+            }
         }
     }
 }
