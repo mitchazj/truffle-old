24c87eb5b26b5d8ce7e49fa8e5333df1792a0d09
Mitchell Johnson <orangutandevelopment@gmail.com>
2016-08-13 12:14:15 +1000

    :flame: Lit changes

--- Blacksink/Blacksink/frmMain.Designer.cs
+++ Blacksink/Blacksink/frmMain.Designer.cs
@@ -33,7 +33,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1021, 740);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
