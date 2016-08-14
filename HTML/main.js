d8973deb397f8466816e6e1a7d5e7c721efc27c2
Mitchell Johnson <orangutandevelopment@gmail.com>
2016-08-14 13:04:47 +1000

    :fire: Adding Firebase

---
+++ HTML/main.js
@@ -0,0 +1,11 @@
+<script src="https://www.gstatic.com/firebasejs/3.2.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBEaesrRCB2xcIPlNF8s2rwCL1sbri7yO0",
+    authDomain: "truffle-91bcf.firebaseapp.com",
+    databaseURL: "https://truffle-91bcf.firebaseio.com",
+    storageBucket: "truffle-91bcf.appspot.com",
+  };
+  firebase.initializeApp(config);
+</script>
